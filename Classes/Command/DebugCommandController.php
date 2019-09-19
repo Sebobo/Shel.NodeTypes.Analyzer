e296c363af910411689a5f152831e0fdc6dd4dba
Sebastian Helzle <sebastian@helzle.it>
2019-09-19 10:27:47 +0200

    FEATURE: Add colors and shapes to nodes based on their type/group

--- Classes/Command/DebugCommandController.php
+++ Classes/Command/DebugCommandController.php
@@ -27,11 +27,25 @@ class DebugCommandController extends CommandController
     protected $nodeTypeManager;
 
     /**
+     * @var array
+     */
+    protected $nodeTypeGroups = [];
+
+    /**
+     * @var array
+     */
+    protected $graphColors = [
+        'blue',
+        'red',
+        'green',
+    ];
+
+    /**
      * @param string|null $baseNodeType
      * @param string|null $format
      * @throws StopActionException
      */
-    public function nodeTypeGraphCommand(?string $baseNodeType = null, ?string $format = null): void
+    public function nodeTypeGraphCommand(?string $baseNodeType = null, ?string $format = null, ?string $filename = 'nodetype-hierarchy'): void
     {
         if ($baseNodeType !== null) {
             try {
@@ -44,83 +58,109 @@ class DebugCommandController extends CommandController
         }
 
         if ($format === 'graphml' || $format === 'graphviz') {
-            $graph = new Graph();
-
-            if ($baseNodeType) {
-                $this->addNodeToGraph($baseNodeType, $graph);
-            } else {
-                $nodeTypes = $this->nodeTypeManager->getNodeTypes();
-
-                /** @var NodeType $nodeType */
-                foreach ($nodeTypes as $nodeType) {
-                    $this->addNodeToGraph($nodeType, $graph);
-                }
-            }
+            $graph = $this->buildGraph($baseNodeType);
 
             if ($format === 'graphviz') {
-                $graphviz = new GraphViz();
-                $data = $graphviz->createScript($graph);
-
-                // Hack for replacing numerical group ids with their actual name as the library doesn't support those yet
-                $groupIds = array_reduce($graph->getVertices()->getMap(), function (array $carry, Vertex $vertex) {
-                    $carry[$vertex->getGroup()] = $vertex->getAttribute('Package');
-                    return $carry;
-                }, []);
-                foreach ($groupIds as $groupId => $groupName) {
-                    $data = str_replace($groupId, '"' . $groupName . '"', $data);
-                }
-
-                // Hack to set rankdir and splines attributes of graph
-                $data = str_replace('digraph G {', 'digraph G {' . PHP_EOL  . '  rankdir=LR;' . PHP_EOL  . '  splines=line;', $data);
-                file_put_contents('nodetype-hierarchy.graphviz', $data);
+                $this->writeGraphVizFile($filename, $graph);
             } else {
-                $exporter = new GraphExporter();
-                $data = $exporter->getOutput($graph);
-                file_put_contents('nodetype-hierarchy.graphml', $data);
+                $this->writeGraphMLFile($filename, $graph);
             }
         } else {
-            if ($baseNodeType) {
-                $this->renderNodeType($baseNodeType);
-            } else {
-                $nodeTypes = $this->nodeTypeManager->getNodeTypes();
-
-                /** @var NodeType $nodeType */
-                foreach ($nodeTypes as $nodeType) {
-                    $this->renderNodeType($nodeType);
-                }
-            }
+            $this->outputGraph($baseNodeType);
         }
     }
 
     /**
+     * Create a graph structure from the nodetype inheritance starting from `baseNodeType`
+     *
+     * @param NodeType|null $baseNodeType
+     * @return Graph
+     */
+    protected function buildGraph(?NodeType $baseNodeType = null): Graph
+    {
+        $graph = new Graph();
+
+        if ($baseNodeType) {
+            $this->addNodeToGraph($baseNodeType, $graph);
+        } else {
+            $nodeTypes = $this->nodeTypeManager->getNodeTypes();
+
+            /** @var NodeType $nodeType */
+            foreach ($nodeTypes as $nodeType) {
+                $this->addNodeToGraph($nodeType, $graph);
+            }
+        }
+        return $graph;
+    }
+
+    /**
+     * Adds a nodetype to the graph if it's not contained yet and continues with it's supertypes
+     *
      * @param NodeType $nodeType
      * @param Graph $graph
+     * @return Vertex
      */
-    protected function addNodeToGraph(NodeType $nodeType, Graph &$graph): void
+    protected function addNodeToGraph(NodeType $nodeType, Graph &$graph): Vertex
     {
         $superTypes = $nodeType->getDeclaredSuperTypes();
 
         if ($graph->hasVertex($nodeType->getName())) {
-            $graphNode = $graph->getVertex($nodeType->getName());
+            return $graph->getVertex($nodeType->getName());
         } else {
             $nameParts = explode(':', $nodeType->getName());
-            $packageParts = explode('.', $nameParts[0]);
-            $graphNode = $graph->createVertex($nodeType->getName());
-            $graphNode->setAttribute('Package', $nameParts[0]);
-            $graphNode->setAttribute('Vendor', $packageParts[0]);
+            $packageName = $nameParts[0];
+            $packageParts = explode('.', $packageName);
+            $vendor = $packageParts[0];
 
-            $hash = md5($nameParts[0]);
+            $graphNode = $graph->createVertex($nodeType->getName());
+
+            // Create unique hash for the group which will be later replaced by the groups name
+            $hash = md5($packageName);
             $groupId = crc32($hash);
+            if (!array_key_exists($groupId, $this->nodeTypeGroups)) {
+                $this->nodeTypeGroups[$groupId] = [
+                    'name' => $packageName,
+                    'color' => $this->graphColors[$groupId % count($this->graphColors)],
+                ];
+            }
+            $group = $this->nodeTypeGroups[$groupId];
+
             $graphNode->setGroup($groupId);
+            $graphNode->setAttribute('graphviz.color', $group['color']);
+            $graphNode->setAttribute('Package', $packageName);
+            $graphNode->setAttribute('Vendor', $vendor);
+
+            if ($nodeType->isAbstract()) {
+                $graphNode->setAttribute('graphviz.shape', 'hexagon');
+            }
+            // TODO: set alternate shape for final nodetypes
         }
 
         /** @var NodeType $superType */
         foreach ($superTypes as $superType) {
-            $this->addNodeToGraph($superType, $graph);
-
-            $superTypeNode = $graph->getVertex($superType->getName());
+            $superTypeNode = $this->addNodeToGraph($superType, $graph);
             if (!$graphNode->hasEdgeTo($superTypeNode)) {
                 $graphNode->createEdgeTo($superTypeNode);
+            }
+        }
+        return $graphNode;
+    }
+
+    /**
+     * Writes the graph structure to the stdout
+     *
+     * @param NodeType|null $baseNodeType
+     */
+    protected function outputGraph(?NodeType $baseNodeType = null): void
+    {
+        if ($baseNodeType) {
+            $this->renderNodeType($baseNodeType);
+        } else {
+            $nodeTypes = $this->nodeTypeManager->getNodeTypes();
+
+            /** @var NodeType $nodeType */
+            foreach ($nodeTypes as $nodeType) {
+                $this->renderNodeType($nodeType);
             }
         }
     }
@@ -140,5 +180,36 @@ class DebugCommandController extends CommandController
         foreach ($superTypes as $superType) {
             $this->renderNodeType($superType, $depth + 2);
         }
+    }
+
+    /**
+     * @param string $filename
+     * @param Graph $graph
+     */
+    protected function writeGraphMLFile(string $filename, Graph $graph): void
+    {
+        $exporter = new GraphExporter();
+        $data = $exporter->getOutput($graph);
+        file_put_contents($filename . '.graphml', $data);
+    }
+
+    /**
+     * @param string $filename
+     * @param Graph $graph
+     */
+    protected function writeGraphVizFile(string $filename, Graph $graph): void
+    {
+        $graph->setAttribute('graphviz.graph.rankdir', 'LR');
+        $graph->setAttribute('graphviz.graph.splines', 'line');
+
+        $graphviz = new GraphViz();
+        $data = $graphviz->createScript($graph);
+
+        // Hack for replacing numerical group ids with their actual name as the library doesn't support those yet
+        foreach ($this->nodeTypeGroups as $groupId => $groupProperties) {
+            $data = str_replace($groupId, '"' . $groupProperties['name'] . '"', $data);
+        }
+
+        file_put_contents($filename . '.graphviz', $data);
     }
 }
